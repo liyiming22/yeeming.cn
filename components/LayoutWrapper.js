904245fceec75c5c13db74c2801999ba568c7173
liyiming <lym.liam@outlook.com>
2021-08-09 23:04:41 +0800

    feat: 🎸 enable SVG components

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,11 +1,24 @@
 import SectionContainer from "./SectionContainer"
+import Link from './Link'
+import Logo from '@/data/logo.svg'
 
 export default function LayoutWrapper({ children }) {
   return (
 		<SectionContainer>
 			<div className="flex flex-col justify-between h-screen">
-				{ children }
+				<header className="flex justify-between items-center py-10">
+					<div>
+						<Link href="/" aria-label="Tailwind CSS Blog">
+							<div className="flex justify-between items-center">
+								<div className="mr-3">
+									<Logo />
+								</div>
+							</div>
+						</Link>
+					</div>
+				</header>
 			</div>
+			<main className="mb-auto">{ children }</main>
 		</SectionContainer>
 	)
 }
